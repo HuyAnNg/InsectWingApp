6df53513f9347ef56e46cf25794d9d4b889a5b27
HuyAnNg <nguyenhuyan27@gmail.com>
2021-05-31 11:32:26 +0700

    Add splash screen

--- InsectWing/App.tsx
+++ InsectWing/App.tsx
@@ -1,8 +1,13 @@
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View } from 'react-native';
+import SplashScreen from 'react-native-splash-screen'
  
  const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  },[])
+
    return <View />;
  };
  
